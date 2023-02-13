6000291206b1daf4539138031e749028fbb5c91e
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-02-13 19:08:51 +0100

    noir

--- src/components/Nuage.vue
+++ src/components/Nuage.vue
@@ -1,7 +1,7 @@
 <template>
   <Renderer class="planet" ref="renderer" antialias :orbit-ctrl="{ enableDamping: true }" resize>
     <Camera :position="{ x: 10, y: 0, z: 0 }" />
-    <Scene background="#fff">
+    <Scene background="#000">
       <AmbientLight></AmbientLight>
       <GltfModel src="models/planet.glb" />
     </Scene>
